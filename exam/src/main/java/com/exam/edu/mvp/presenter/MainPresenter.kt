f27c5bdf7a50b82b7a9095836991f1df38f24a14
liping <626228379@qq.com>
2020-09-18 18:24:07 +0800

    Initial commit

---
+++ exam/src/main/java/com/exam/edu/mvp/presenter/MainPresenter.kt
@@ -0,0 +1,48 @@
+package com.exam.edu.mvp.presenter
+
+import android.app.Application
+
+import com.jess.arms.integration.AppManager
+import com.jess.arms.di.scope.ActivityScope
+import com.jess.arms.mvp.BasePresenter
+import com.jess.arms.http.imageloader.ImageLoader
+import me.jessyan.rxerrorhandler.core.RxErrorHandler
+import javax.inject.Inject
+
+import com.exam.edu.mvp.contract.MainContract
+
+
+/**
+ * ================================================
+ * Description:
+ * <p>
+ * Created by MVPArmsTemplate on 09/18/2020 17:41
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms">Star me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArms/wiki">See me</a>
+ * <a href="https://github.com/JessYanCoding/MVPArmsTemplate">模版请保持更新</a>
+ * ================================================
+ */
+@ActivityScope
+class MainPresenter
+@Inject
+constructor(model: MainContract.Model, rootView: MainContract.View) :
+        BasePresenter<MainContract.Model, MainContract.View>(model, rootView) {
+    @Inject
+    lateinit var mErrorHandler: RxErrorHandler
+
+    @Inject
+    lateinit var mApplication: Application
+
+    @Inject
+    lateinit var mImageLoader: ImageLoader
+
+    @Inject
+    lateinit var mAppManager: AppManager
+
+
+    override fun onDestroy() {
+        super.onDestroy();
+    }
+}
